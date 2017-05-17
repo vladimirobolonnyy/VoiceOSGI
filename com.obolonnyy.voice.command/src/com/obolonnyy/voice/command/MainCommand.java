0e0210c1b7ba2d5b7f1dbcdd62ad8df9c0021ce3
Vladimir <obolonnyyv@gmail.com>
2017-05-17 16:04:14 +0300

    Пытаемся сделать отдельные компоненты для микрофона, гугл респонсера и расчипукивателя. Пока что из потока RecordingThread не создаётся микрофон =(

--- com.obolonnyy.voice.command/src/com/obolonnyy/voice/command/MainCommand.java
+++ com.obolonnyy.voice.command/src/com/obolonnyy/voice/command/MainCommand.java
@@ -3,56 +3,83 @@ package com.obolonnyy.voice.command;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
-import java.io.File;
-import java.util.ArrayList;
-
 import org.apache.felix.service.command.CommandProcessor;
+
+import com.obolonny.voice.recordingAPI.recordingAPI;
+
 import com.obolonnyy.voice.myVoiceAPI;
 
-@Component(
-	    property= {
-	        CommandProcessor.COMMAND_SCOPE + ":String=fipro",
-	        CommandProcessor.COMMAND_FUNCTION + ":String=main",
-	        CommandProcessor.COMMAND_FUNCTION + ":String=test"
-	    },
-	    service=MainCommand.class
-	)
+@Component(property = {
+		CommandProcessor.COMMAND_SCOPE + ":String=fipro",
+		CommandProcessor.COMMAND_FUNCTION + ":String=main",
+		CommandProcessor.COMMAND_FUNCTION + ":String=test",
+		CommandProcessor.COMMAND_FUNCTION + ":String=test2"
+		},
+	service = MainCommand.class
+)
+
 public class MainCommand {
-	   private myVoiceAPI voice;
-
-	    @Reference
-	    void bindStringvoice(myVoiceAPI voice) {
-	        this.voice = voice;
-	    }
-
-	    void unbindStringvoice(myVoiceAPI voice) {
-	    	this.voice = null;
-	    }
 
 
-	    public void test() {
-	    	System.out.println("Test is working");
-	    	voice.TestGoogle();
-	    }
+	private recordingAPI voice2;
 
-
-	public void main() {
-		System.out.println("Main is working");
-
-		ArrayList<String> phrases = new ArrayList<>();
-		File audioFile;
-
-		for (int i = 0; i < 1; i++) {
-
-			audioFile = voice.runRecording();
-			phrases = voice.sendRecordToGoogle(audioFile);
-
-			for (String phrase : phrases) {
-				System.out.println(phrase);
-			}
-
-			phrases = new ArrayList<>();
-		}
-
+	@Reference
+	void bindStringvoice(recordingAPI voice2) {
+		this.voice2 = voice2;
 	}
+
+	void unbindStringvoice(recordingAPI voice) {
+		this.voice2 = null;
+	}
+
+	public void test() {
+		System.out.println("Test is working");
+		voice2.startRecording();
+	}
+
+
+	 private myVoiceAPI voice;
+
+	 //@Reference void bindStringvoice(myVoiceAPI voice) { this.voice = voice; }
+
+	 //void unbindStringvoice(myVoiceAPI voice) { this.voice = null; }
+
+
+
+/*	private ComponentContext context;
+    @Activate
+    void activate(ComponentContext context) {
+        this.context = context;
+        System.out.println("done");
+    }*/
+
+/*	public void test2() {
+		System.out.println("Test2 is working");
+		voice2.startRecording();
+	}*/
+
+//	public void test2() {
+//		System.out.println("Test is working");
+//		voice.TestGoogle();
+//	}
+
+//	public void main() {
+//		System.out.println("Main is working");
+//
+//		ArrayList<String> phrases = new ArrayList<>();
+//		File audioFile;
+//
+//		for (int i = 0; i < 1; i++) {
+//
+//			audioFile = voice.runRecording();
+//			phrases = voice.sendRecordToGoogle(audioFile);
+//
+//			for (String phrase : phrases) {
+//				System.out.println(phrase);
+//			}
+//
+//			phrases = new ArrayList<>();
+//		}
+//
+//	}
 }
---
+++ com.obolonnyy.voice.command/src/com/obolonnyy/voice/command/WaitResultThread.java
@@ -0,0 +1,10 @@
+package com.obolonnyy.voice.command;
+
+public class WaitResultThread extends Thread{
+
+	@Override
+	public void run() {
+
+
+	}
+}
